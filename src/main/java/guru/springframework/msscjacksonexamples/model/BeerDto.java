914480ec587b586d62355047f38460e0ebbba19a
Marc <marc.marechal@dell.com>
2021-03-02 10:24:44 +0800

    Serialize price to String and change date format. Closes #6

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscjacksonexamples.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,7 +36,10 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 }
